a3b0f64f81c3987d8f3a2efe0094f868aa131877
Sho Sakakibara <sho@sakakibara.dev>
2023-10-17 16:47:32 +0900

    Add ignore whitespace to git apply option

--- scripts/tools.sh
+++ scripts/tools.sh
@@ -81,7 +81,7 @@ function apply_patches ()
     if test -d "$source_dir"; then
         echo Applying patches in $patches_dir
         cd $source_dir
-        find $patches_dir/*.patch | xargs -I % git apply %
+        find $patches_dir/*.patch | xargs -I % git apply --ignore-whitespace %
         error=$?
     fi
 
